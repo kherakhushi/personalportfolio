569287d2523456e8a39f560e4656f8d73e036f0a
kherakhushi <kherakhushi317@gmail.com>
2024-09-25 22:40:16 +0530

    Update admin-auth.php

--- admin-auth.php
+++ admin-auth.php
@@ -12,16 +12,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Check if form data was submitted
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
 
-    // Hardcoded credentials
     $admin_username = "khushi";
     $admin_password = "215512";
-
-    // Validate credentials
     if ($username === $admin_username && $password === $admin_password) {
         // Set session variable and redirect to admin panel
         $_SESSION['loggedin'] = true;
@@ -33,7 +30,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
     echo "<p>Invalid request.</p>";
 }
-
-// Close the database connection
 $conn->close();
 ?>
