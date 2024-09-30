3f124c73c4a8a4a161012a99b3a96308b7800576
khushi <kherakhushi317@gmail.com>
2024-09-30 10:32:19 +0530

    Update admin-auth.php

--- admin-auth.php
+++ admin-auth.php
@@ -4,15 +4,10 @@ $servername = "localhost";
 $username = "root"; 
 $password = ""; 
 $dbname = "portfolio_db"; 
-
-
 $conn = new mysqli("localhost", "root", "", "portfolio_db");
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
@@ -20,7 +15,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $admin_username = "khushi";
     $admin_password = "215512";
     if ($username === $admin_username && $password === $admin_password) {
-        // Set session variable and redirect to admin panel
         $_SESSION['loggedin'] = true;
         header("location: admin-panel.php");
         exit;
