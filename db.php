ee45ef632100fa3ceee8a8ad0740a729202b268f
khushi <kherakhushi317@gmail.com>
2024-10-03 11:28:09 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,8 @@
 <?php
 $host = 'localhost';
 $db = 'portfolio_db';
-$user = 'root'; // Change this if your username is different
-$pass = ''; // Change this if you have a password
-
+$user = 'root'; 
+$pass = ''; 
 $conn = new mysqli("LOCALHOST","root","","portfolio_db");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
